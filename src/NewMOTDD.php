db8e82c6086452f32317698b5cf52a2121810e61
Intel1999 <805473989@qq.com>
2016-04-04 13:23:42 +0800

    Create NewMOTDD.php

---
+++ src/NewMOTDD.php
@@ -0,0 +1,28 @@
+<?php
+namespace NewMOTD;
+
+
+use pocketmine\scheduler\PluginTask;
+use pocketmine\utils\TextFormat;
+
+class NewMOTDD extends PluginTask
+{
+    private $plugin;
+
+    public function __construct(NewMOTD $plugin)
+    {
+        parent::__construct($plugin);
+        $this->plugin = $plugin;
+    }
+
+    public function onRun($currentTick)
+    {
+		$logo = array(" §7Hello!"," §7Welcome!"," §7Have a good time!"," §7We will be better!"," §7Love our server!"," §7QQgroup:249849836!");//自行添加
+		$logo2 = array_rand($logo);
+        $motd = $this->plugin->getServer()->getMotd();
+        $motd = TextFormat::AQUA . $motd;
+        $motd .= $logo[$logo2];
+        $this->plugin->getServer()->getNetwork()->setName($motd);
+    }
+}
+?>

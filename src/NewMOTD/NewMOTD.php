092a166cf81e90215d5abd2a717dcb7e8383986b
Intel1999 <805473989@qq.com>
2016-04-04 22:55:45 +0800

    Update NewMOTD.php

--- src/NewMOTD/NewMOTD.php
+++ src/NewMOTD/NewMOTD.php
@@ -11,8 +11,13 @@ class NewMOTD extends PluginBase
     public function onEnable()
     {
 		$this->getlogger()->info(TextFormat::GREEN."Welcome to use the plugin,the plugin by Intel~");
-		$this->getlogger()->info(TextFormat::GREEN."version : 1.1");
+		$this->getlogger()->info(TextFormat::GREEN."version : 1.2");
 		$this->getlogger()->info(TextFormat::GREEN."NewMOTD loaded !");//
-    $this->getServer()->getScheduler()->scheduleRepeatingTask(new NewMOTDD($this), 20);//20 seconds refresh
+                $this->getServer()->getScheduler()->scheduleRepeatingTask(new NewMOTDD($this), 20);//20 seconds refresh
+              /*@mkdir($this->getDataFolder());
+		$this->config = new Config($this->getDataFolder()."config.yml", Config::YAML, array(
+		"是否开启显示"=>"true"));
+		$turn = $this->config->get("是否开启显示");
+		$turn2 = $this->config->get("宣传语");*/
     }
 }

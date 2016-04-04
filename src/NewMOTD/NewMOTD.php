feef532900b22b4e3763010d90c86df62faf1520
Intel1999 <805473989@qq.com>
2016-04-04 17:01:55 +0800

    Create NewMOTD.php

---
+++ src/NewMOTD/NewMOTD.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace NewMOTD;
+
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+
+class NewMOTD extends PluginBase
+{
+    public function onEnable()
+    {
+		$this->getlogger()->info(TextFormat::GREEN."Welcome to use the plugin,the plugin by Intel~");
+		$this->getlogger()->info(TextFormat::GREEN."version : 1.1");
+		$this->getlogger()->info(TextFormat::GREEN."NewMOTD loaded !");
+    $this->getServer()->getScheduler()->scheduleRepeatingTask(new NewMOTDD($this), 20);//20 seconds refresh
+    }
+}
